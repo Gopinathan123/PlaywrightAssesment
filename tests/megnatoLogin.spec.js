c5cc9671b33649e58e1022b57fc640d2788dd018
Gopinathan S <137474118+Gopinathan123@users.noreply.github.com>
2025-11-20 13:13:55 +0530

    Refactor test descriptions for clarity and tagging

--- tests/megnatoLogin.spec.js
+++ tests/megnatoLogin.spec.js
@@ -2,8 +2,8 @@ import { test } from '@playwright/test';
 import cred from '../testData/testData.json' with {type:'json'};
 import MegnatoPage from '../pageObject/megnatoLogin.page';
 
-test.describe('Verifing Login with valid and invalid credentials @smoke`, async () => {
-    test("Megnato valid Cred", async ({ page }) => {
+test.describe('Verifing Login with valid and invalid credentials`, async () => {
+    test(`Megnato valid Cred`,{tag: ['@smoke']}, async ({ page }) => {
         let signinPage = new MegnatoPage(page);
         await signinPage.navigateToUrl(cred.url);
         await signinPage.clickSigninFromHome();
@@ -13,7 +13,7 @@ test.describe('Verifing Login with valid and invalid credentials @smoke`, async 
         await signinPage.signoutAndAssert();
         await page.close();
     });
-    test(`Megnato invalid Cred @smoke`, async ({ page }) => {
+    test(`Megnato invalid Cred`,{tag: ['@smoke']}, async ({ page }) => {
         let signinPage = new MegnatoPage(page);
         await signinPage.navigateToUrl(cred.url);
         await signinPage.clickSigninFromHome();
